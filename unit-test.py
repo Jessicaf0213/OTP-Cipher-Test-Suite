b7df7c04bbd048a1a3957dcaecd27ede73f62f17
Jessica Feng <jessica@Jessicas-MacBook-Pro.local>
2022-02-20 05:17:51 -0500

    Create a unit test for cipher.txt

---
+++ unit-test.py
@@ -0,0 +1,41 @@
+from cipher import *
+import os.path
+import random
+
+# test genereatPad
+def test_generatePad_by_random_size():
+    # generate OTP in random size (50-1500)
+    generatePad(random.randrange(50,1500,1))
+    # pass test if a OTP pad file exists
+    assert os.path.exists("OTP.txt")
+
+# Read the decrypted msg in the folder
+with open("decrypted-message.txt") as file: 
+    plaintxt = file.read()
+# read the pad in the folder
+with open("pad.txt") as file:
+    pad = file.read()
+# read the encrpted msg in the foler
+with open("encrypted-message.txt") as file: 
+    entxt = file.read()
+
+def test_encipher(): 
+    # encrypt the plaintext using the given pad file
+    assert encipher(pad, plaintxt) == entxt
+
+def test_decipher():
+    # decrypt the encrypted msg using the given pad file
+    assert decipher(pad, entxt) == plaintxt
+
+def test_encipher_shift_letters_by_1():
+    assert encipher("AAAAA", "ABCDE") == "BCDEFG"
+
+def test_decipher_shift_letters_by_1():
+    assert decipher("AAAAA", "BCDEFG") == "ABCDE"
+
+def test_encipher_shift_letters_by_25():
+    assert encipher("YYYYY", "A! BCDE") == "Z! ABCD"
+
+def test_decipher_shift_letters_by_25():
+    assert decipher("YYYYY", "Z ?ABC D") == "A ?BCD E"
+
